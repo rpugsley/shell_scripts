08a68491733aac86a4de6d762c4ddd74c05d8903
rpugsley <rpugsley@gmail.com>
2023-11-18 12:00:06 -0500

    Update detect_dhcp.sh

--- detect_dhcp.sh
+++ detect_dhcp.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/usr/local/bin/bash
 
 # Define the email address to which the email should be sent
 EMAIL="email@example.com"
@@ -28,15 +28,17 @@ while true; do
             MAC=$(echo "$LAST_LINE" | awk '{print $13}')
             ETH=$(echo "$LAST_LINE" | awk '{print $15}')
             if  ! grep -qi "$MAC" "$STATIC_DHCP_FILE"; then
-                # Run the nmap command and store the output in a variable
-                OUTPUT=$(nmap -sP "$IP")
-                # Extract the hostname from the output using grep and cut
-                HOSTNAME=$(echo "$OUTPUT" | grep -m 1 "Nmap scan report for" | cut -d " " -f 5)
+		# Run the nmap command and store the output in a variable
+		OUTPUT=$(nmap -sP "$IP")
+		# Extract the hostname from the output using grep and cut
+		HOSTNAME=$(echo "$OUTPUT" | grep -m 1 "Nmap scan report for" | cut -d " " -f 5)
                 echo "$LAST_LINE" > /tmp/last_match
-                BODY=`printf "A new DHCP release has been detected for an unknown device. The details are as follows:\n\nOn $(date)\nIP Address: $IP\nMAC Address: $MAC\nHostname: $HOSTNAME\n"`                echo -e "$BODY"
-                #echo -e "$BODY" | mail -s "$SUBJECT" "$EMAIL"
+                BODY=`printf "A new DHCP release has been detected for an unknown device. The details are as follows:\n\nOn $(date)\nIP Address: $IP\nMAC Address: $MAC\nHostname: $HOSTNAME\n"`
+                echo -e "$BODY"
+                sleep 3
                 python3 /root/sendmail.py -s "$SUBJECT" -a "$BODY"
             fi
         fi
     fi
 done
+
